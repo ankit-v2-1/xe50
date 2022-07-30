5784e84ee95b434e31847a7c74d79f81cbfbe8ad
Ankit raj <ankitjaiswal7890@gmail.com>
2022-07-30 12:39:06 +0530

    build: add bap

---
+++ bap/server.ts
@@ -0,0 +1,79 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'cross-fetch';
+import crypto from 'crypto';
+var proxy = require('express-http-proxy');
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/proxy', proxy('www.google.com'));
+
+
+
+var transaction_id = crypto.randomBytes(16).toString("hex");
+var message_id = crypto.randomBytes(16).toString("hex");
+
+
+const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        "context": {
+            "city": "std:080",
+            "action": "search",
+            "bap_id": "https://b913-2405-204-5207-ff18-9071-a6a7-87ee-7b03.in.ngrok.io/",
+            "domain": "nic2004:52110",
+            "bap_uri": "https://b913-2405-204-5207-ff18-9071-a6a7-87ee-7b03.in.ngrok.io/callback",
+            "country": "IND",
+            "timestamp": "2022-07-28T08:00:48.462Z",
+            "message_id": message_id,
+            "core_version": "1.0.0",
+            "transaction_id": transaction_id
+        },
+        "message": {
+            "intent": {
+                "payment": {
+                    "@ondc/org/buyer_app_finder_fee_type": "percent",
+                    "@ondc/org/buyer_app_finder_fee_amount": 3
+                },
+                "fulfillment": {
+                    "type": "Delivery"
+                }
+            }
+        }
+    }
+    )
+}
+
+const url = 'https://api.test.esamudaay.com/external/v1/ondc/retail/bpp/search';
+
+
+fetch(url + "?wait=true", requestOptions)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+    });
+
+
+var callbackData: string;
+
+
+app.post('/callback/on_search', (req, res) => {
+    console.log(req.body.message);
+    callbackData = req.body;
+});
+
+
+
+app.get('/search', (req, res) => {
+    res.send(callbackData);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Bap listening on port 3000!');
+});
--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,17 @@ function App() {
   const [data, setData] = useState('null');
 
   const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      "context": {
-        "city": "std:080",
-        "action": "search",
-        "bap_id": "ondc.paytm.com",
-        "domain": "nic2004:52110",
-        "bap_uri": "https://ondc.paytm.com/retail",
-        "country": "IND",
-        "timestamp": "2022-07-28T08:00:48.462Z",
-        "message_id": "1658995248462",
-        "core_version": "1.0.0",
-        "transaction_id": "ce128f2e6c43-4af3-aa2f-ebb61161a9b4"
-      },
-      "message": {
-        "intent": {
-          "payment": {
-            "@ondc/org/buyer_app_finder_fee_type": "percent",
-            "@ondc/org/buyer_app_finder_fee_amount": 3
-          },
-          "fulfillment": {
-            "type": "Delivery"
-          }
-        }
-      }
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
     }
-    )
   }
 
-  const url = 'https://api.test.esamudaay.com/external/v1/ondc/retail/bpp/search';
+  const url = 'https://b913-2405-204-5207-ff18-9071-a6a7-87ee-7b03.in.ngrok.io/search';
 
   useEffect(() => {
-    fetch(url, requestOptions)
-      .then(async res => await res.json())
+    fetch(url + "?wait=true", requestOptions)
+      .then(res => res.json())
       .then(data => {
         console.log(data);
         setData(data)
